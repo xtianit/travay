f5fec858d422cfad2d071a2cbd80a7a04980d550
Gwenael Pluchon <accounts@gwenp.fr>
2018-08-25 20:25:14 +0200

    fix DatePicker component

--- src/main.js
+++ src/main.js
@@ -11,7 +11,24 @@ import VueI18n from 'vue-i18n'
 import VueUI from 'vue-ui/dist/vue-ui.common';
 import 'vue-ui/dist/vue-ui.css';
 
+import enlocale from '../i18n/en.json';
+
 Vue.use(VueI18n)
+
+const messages = {
+  en: enlocale
+}
+
+// Create VueI18n instance with options
+const i18n = new VueI18n({
+  locale: 'en', // set locale
+  messages, // set locale messages
+})
+
+window['$d'] = i18n.d.bind(i18n);
+window['$t'] = i18n.t.bind(i18n);
+window['$i18n'] = i18n;
+
 Vue.use(VueUI);
 Vue.config.productionTip = false
 
