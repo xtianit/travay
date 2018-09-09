d07ba1dcd970893415cb84d47cf827b600c3cd8e
Farah Brunache <farah.brunache>
2018-09-09 16:42:31 -0400

    added kreyol translations

--- src/components/home.vue
+++ src/components/home.vue
@@ -8,45 +8,51 @@
     </vue-grid-row>
     <vue-grid-row>
       <vue-grid-item>
-        <h2>Earn Stable Income</h2>
-        <p>Same Day Jobs Marketplace</p>
-        <p>Travay provides a citizen driven, long-term jobs market, helping to provide to economic mobility.</p>
+        <h2>{{ $t('App.home.earn' /* Earn Stable Income */) }}</h2>
+        <p>{{ $t('App.home.sameDayJobs' /* Same Day Jobs Marketplace */) }}</p>
+        <p>{{ $t('App.home.sameDayJobsDescription' /* Travay provides a citizen driven, long-term jobs market, helping
+          to provide to economic mobility. */) }}</p>
       </vue-grid-item>
     </vue-grid-row>
     <vue-grid-row>
       <vue-grid-item>
-        <h2>Receive Instant Payments</h2>
-        <p>Jobs Paid in Cryptocurrency</p>
-        <p>Cryptocurrency Payment = Instant Access to Your Money.</p>
+        <h2>{{ $t('App.home.receivePayments' /* Receive Instant Payments */) }}</h2>
+        <p>{{ $t('App.home.jobsPaidInCrypto' /* Jobs Paid in Cryptocurrency */) }}</p>
+        <p>{{ $t('App.home.crypto' /* Cryptocurrency Payment = Instant Access to Your Money. */) }}</p>
       </vue-grid-item>
     </vue-grid-row>
     <vue-grid-row>
       <vue-grid-item>
-        <h2>How Does It Work?</h2>
+        <h2>{{ $t('App.home.howDoesItWork' /* How Does It Work? */) }}</h2>
         <ol>
-          <li>Register by signing in</li>
-          <li>Find a job your want and click claim</li>
-          <li>When work is complete, upload your proof of work and click complete job</li>
-          <li>Your Job Manager will approve work and issue payment</li>
+          <li>{{ $t('App.home.register' /* Register by signing in */) }}</li>
+          <li>{{ $t('App.home.findAndClaim' /* Find a job your want and click claim */) }}</li>
+          <li>{{ $t('App.home.whenCompleteInstructions' /* When work is complete, upload your proof of work and click
+            complete job */) }}</li>
+          <li>{{ $t('App.home.mangerApproves' /* Your Job Manager will approve work and issue payment */) }}</li>
         </ol>
       </vue-grid-item>
     </vue-grid-row>
     <vue-grid-row>
       <vue-grid-item>
-        <h2>How to Get Started Fast</h2>
-        <p>Find a job, claim it with no experience needed!</p>
+        <h2>{{ $t('App.home.getStartedFast' /* How to Get Started Fast */) }}</h2>
+
+        <p>{{ $t('App.home.noExperienceNeed' /* Find a job, claim it with no experience needed! */) }}</p>
         <br>
         <ol>
-          <li>Visit this website in the <a
+          <li>{{ $t('App.getStarted.install' /* Install */) }} <a
             href="https://play.google.com/store/apps/details?id=org.mozilla.firefox&hl=fr" target="_blank">Firefox</a>
-            browser.
+            {{ $t('App.getStarted.browser' /* browser */) }}.
           </li>
-          <li>Log into Travay with your Gmail Account. Don't have one? Sign up <a
+          <li>{{ $t('App.getStarted.step2' /* Log into Travay with your Gmail Account. Don't have one? Sign up */) }} <a
             href="https://accounts.google.com/signup/v2/webcreateaccount?continue=https%3A%2F%2Faccounts.google.com%2FManageAccount&hl=fr&flowName=GlifWebSignIn&flowEntry=SignUp"
-            target="_blank">here</a>.
+            target="_blank">{{ $t('App.getStarted.here' /* here */) }}</a>.
           </li>
-          <li>Install <a href="https://addons.mozilla.org/en-US/firefox/addon/ether-metamask"
-                         target="_blank">MetaMask</a> Firefox extension.
+          <li>{{ $t('App.getStarted.install' /* Install */) }} <a href="https://addons.mozilla.org/en-US/firefox/addon/ether-metamask"
+                                                                  target="_blank">MetaMask</a> Firefox {{ $t('App.getStarted.extension' /* extension */) }}.
+          </li>
+          <li>
+            {{ $t('App.getStarted.startJob' /* Pick a job and start work immediately! */) }}
           </li>
         </ol>
       </vue-grid-item>
