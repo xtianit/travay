58aea8008a5e7e65565db2df004e1acbade784ca
Farah Brunache <farah.brunache>
2018-09-29 23:07:43 -0400

    added a question to home page on text notificaitons

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,20 +1,23 @@
 <template>
   <vue-grid>
+
     <vue-grid-row>
       <vue-grid-item>
         <h1>Travay</h1>
         <p>{{ $t('App.core.description' /* The Long-Term Jobs Market of Haiti */)}}</p><br>
-          <p>{{ $t('App.home.sameDayJobs' /* Same Day Jobs Marketplace */) }}</p>
-          <!--<li>{{ $t('App.home.sameDayJobsDescription' /* Travay provides a citizen driven, long-term jobs market,-->
-            <!--helping-->
-            <!--to provide to economic mobility. */) }}</li>-->
+        <p>{{ $t('App.home.sameDayJobs' /* Same Day Jobs Marketplace */) }}</p>
+        <!--<li>{{ $t('App.home.sameDayJobsDescription' /* Travay provides a citizen driven, long-term jobs market,-->
+        <!--helping-->
+        <!--to provide to economic mobility. */) }}</li>-->
       </vue-grid-item>
     </vue-grid-row>
+
     <vue-grid-row>
       <vue-grid-item>
         <!--<h2>{{ $t('App.home.earn' /* Earn Stable Income */) }}</h2>-->
       </vue-grid-item>
     </vue-grid-row>
+
     <vue-grid-row>
       <vue-grid-item>
         <h2>{{ $t('App.home.receivePayments' /* Receive Instant Payments */) }}</h2>
@@ -22,6 +25,7 @@
         <!--<p>{{ $t('App.home.crypto' /* Cryptocurrency Payment = Instant Access to Your Money. */) }}</p>-->
       </vue-grid-item>
     </vue-grid-row>
+
     <vue-grid-row>
       <vue-grid-item>
         <h2>{{ $t('App.home.howDoesItWork' /* How Does It Work? */) }}</h2>
@@ -29,11 +33,13 @@
           <li>{{ $t('App.home.register' /* Register by signing in */) }}</li>
           <li>{{ $t('App.home.findAndClaim' /* Find a job your want and click claim */) }}</li>
           <li>{{ $t('App.home.whenCompleteInstructions' /* When work is complete, upload your proof of work and click
-            complete job */) }}</li>
+            complete job */) }}
+          </li>
           <li>{{ $t('App.home.mangerApproves' /* Your Job Manager will approve work and issue payment */) }}</li>
         </ol>
       </vue-grid-item>
     </vue-grid-row>
+
     <vue-grid-row>
       <vue-grid-item>
         <h2>{{ $t('App.home.getStartedFast' /* How to Get Started Fast */) }}</h2>
@@ -47,12 +53,12 @@
           </li>
           <li>{{ $t('App.getStarted.installExtension' /* extension */) }} <a
             href="https://addons.mozilla.org/en-US/firefox/addon/ether-metamask"
-                                                                  target="_blank">MetaMask</a>.
+            target="_blank">MetaMask</a>.
           </li>
           <li>{{ $t('App.getStarted.step2' /* Log into Travay with your Gmail Account. Don't have one? Sign up
             */) }} <a
-            href="https://accounts.google.com/signup/v2/webcreateaccount?continue=https%3A%2F%2Faccounts.google.com%2FManageAccount&hl=fr&flowName=GlifWebSignIn&flowEntry=SignUp"
-            target="_blank">{{ $t('App.getStarted.here' /* here */) }}</a>.
+              href="https://accounts.google.com/signup/v2/webcreateaccount?continue=https%3A%2F%2Faccounts.google.com%2FManageAccount&hl=fr&flowName=GlifWebSignIn&flowEntry=SignUp"
+              target="_blank">{{ $t('App.getStarted.here' /* here */) }}</a>.
           </li>
           <li>
             {{ $t('App.getStarted.startJob' /* Pick a job and start work immediately! */) }}
@@ -61,6 +67,15 @@
         <p>{{ $t('App.getStarted.note' /* Note: Travay is only available with Android and Firefox. */) }}</p>
       </vue-grid-item>
     </vue-grid-row>
+
+    <vue-grid-row>
+      <vue-grid-item>
+        <h2>{{ $t('App.home.howToKnowOfNewJobs' /* How Will I Know When There Are New Jobs? */) }}</h2>
+        <p><a href="https://airtable.com/shrGJhkEk4SphwEXe" target="_blank">{{ $t('App.footer.getNotified' /* Get Notified of
+          New Jobs */) }}</a></p>
+      </vue-grid-item>
+    </vue-grid-row>
+
     <br>
   </vue-grid>
 </template>
