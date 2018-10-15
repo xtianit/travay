3c6530398f8f9a0f9702329b1839ca99a4265343
Farah Brunache <farah.brunache>
2018-10-14 23:11:42 -0400

    added translations for pricing page

--- src/components/pricing.vue
+++ src/components/pricing.vue
@@ -3,19 +3,20 @@
     <vue-grid-row>
 
       <vue-grid-item>
-        <h1>Always Transparent Pricing</h1>
+        <h1>{{$t('App.pricing.pageTitle' /* Always Transparent Pricing */) }}</h1>
       </vue-grid-item>
 
       <vue-grid-item>
         <br>
         <vue-panel>
-          <vue-panel-header title="Best Deal" subtitle="You Got This"/>
+          <vue-panel-header :title="$t('App.pricing.2PercentPanelTitle' /* Best Deal */)"
+                            :subtitle="$t('App.pricing.2PercentPanelSubtitle' /* You got this. */)"/>
           <vue-panel-body>
             <h2>2%</h2>
-            <p>One-Time Percentage Fee of Total Salary in USD</p>
+            <p>{{$t('App.pricing.2PercentPara1' /* One-Time Percentage Fee of Total Salary in USD. */) }}</p>
             <br>
-            <p>You post a job and individuals directly select your job. When you fund a job, 2% of the total amount
-              entered for the salary is auto deducted and the worker receives the remaining 98%. We receive the 2%.</p>
+            <p>{{$t('App.pricing.2PercentPara2' /* You post a job and individuals directly select your job. When you fund a job, 2% of the total amount
+              entered for the salary is auto deducted and the worker receives the remaining 98%. We receive the 2%. */) }}</p>
           </vue-panel-body>
           <vue-panel-footer>
             <router-link to="createJob">
@@ -30,22 +31,25 @@
 
       <vue-grid-item>
         <vue-panel>
-          <vue-panel-header title="Good Deal" subtitle="We do the work"/>
+          <vue-panel-header :title="$t('App.pricing.30PanelTitle' /* Good Deal */)"
+                            :subtitle="$t('App.pricing.30PanelSubtitle' /* We do the work */)"></vue-panel-header>
           <vue-panel-body>
-            <h2>$30</h2>
-            <p>USD Flat Fee</p>
+            <h2>$ 30 USD</h2>
             <br>
-            <p>We post and recruit for your job position.</p>
+            <p>{{$t('App.pricing.30Para1' /* We post and recruit for your job position. */) }}</p>
             <br>
-            <p>Ready to get started?</p>
+            <p>{{$t('App.pricing.30Para2' /* Ready to get started? */) }}</p>
             <br>
             <ol>
-              <li><a rel="noopener noreferrer" href="https://www.paypal.me/farahb" target="_blank">Send a $30 payment
-                and the salary for the job. The 2% fee above still applies on the salary.</a></li>
-              <li>Send us an email with your job details to travayht@gmail.com</li>
-              <li>We'll notify you when the job is posted and there are candidates available for your job.</li>
+              <li><a rel="noopener noreferrer" href="https://www.paypal.me/farahb"
+                     target="_blank">{{$t('App.pricing.30bullet1' /* Click here and send a $30 payment with the salary for the job. The 2% fee above still applies on the salary. */) }}</a></li>
+              <li><a href="https://airtable.com/shr9JeDUKyfrq6KRr" target="_blank">{{$t('App.pricing.30bullet2' /*
+                Click here to complete a form with your job details. */) }}</a></li>
+              <li>{{$t('App.pricing.30bullet3' /* We'll notify you when the job is posted and there are candidates
+                available for your job. */) }}</li>
             </ol>
-            <p>Note: the one-time $30 fee is non-refundable.</p>
+            <br>
+            <p>{{$t('App.pricing.30Para3' /* Note: the one-time $30 fee is non-refundable. */) }}</p>
           </vue-panel-body>
           <vue-panel-footer>
           </vue-panel-footer>
